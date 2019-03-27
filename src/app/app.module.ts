85e5bd6c811746c188f3edf7f0f9c2daaa131826
Quy Le <Quy.Le@AnchorGlass.com>
2019-03-26 22:02:02 -0400

    wiring up formcontrols for financial compoments

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -42,7 +43,9 @@ import { FinancialComponent } from './financial/financial.component';
     HighchartsChartModule,
     SharedModule,
     AgGridModule.withComponents([]),
-    NgbModule
+    NgbModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/financial/financial.component.ts
+++ src/app/financial/financial.component.ts
@@ -1,13 +1,29 @@
 import { Component, OnInit } from '@angular/core';
+import { SymbolService } from '../service/symbol.service';
+import { FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-financial',
   templateUrl: './financial.component.html',
-  styleUrls: ['./financial.component.scss']
+  styleUrls: ['./financial.component.scss'],
+  providers: [SymbolService]
 })
 export class FinancialComponent implements OnInit {
 
-  constructor() { }
+  pageName = 'Financial Statements';
+
+  constructor(private symbolService: SymbolService) { }
+
+  symbolName = new FormControl('');
+
+  findSymbol() {
+    this.symbolService.getStockDividend(this.symbolName.value)
+      .subscribe((res)=>{
+        console.log(res);
+    });
+  }
+
+
 
   ngOnInit() {
   }
